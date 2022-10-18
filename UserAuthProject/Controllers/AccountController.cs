9e0ab76305c9bbf953aa4c3250898c16153e2b3b
Sedat <Sedat@DESKTOP-SATO7G7>
2022-10-19 00:06:24 +0300

    Add project files.

---
+++ UserAuthProject/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAuthProject.Models;
+
+namespace UserAuthProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Login İşlemleri
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
